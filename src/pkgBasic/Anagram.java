e4a013fa1077a2ac9f0331b1482ac7bd52934f18
Aswin Gokulachandran <aswin.gokulachandran87@gmail.com>
2015-08-02 00:54:23 -0400

    Anagram.java

--- src/pkgBasic/Anagram.java
+++ src/pkgBasic/Anagram.java
@@ -3,6 +3,9 @@
  */
 package pkgBasic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //import pkgBasic.PermuteString;
 /**
  * @author Aswin
@@ -49,6 +52,30 @@ public class Anagram {
 				
 		}
 	}
+	
+	public boolean isAnagram(String s, String t) {
+        if(s == null && t == null)
+            return true;
+        
+        if(s.length() != t.length())
+            return false;
+        
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        for(int i=0; i<s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }else
+                map.put(s.charAt(i),1);
+        }
+        
+        for(int i=0; i<t.length();i++){
+            if(!map.containsKey(t.charAt(i)) || map.get(t.charAt(i)) <=0)
+                return false;
+            else
+                map.put(t.charAt(i),map.get(t.charAt(i))-1);
+        }
+        return true;
+    }
 	
 	public static void main(String args[]){
 		Anagram obj = new Anagram();
