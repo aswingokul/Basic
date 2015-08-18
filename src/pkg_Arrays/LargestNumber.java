34e52287a320a48eb092a36d6e03faf915fa1e45
Aswin Gokulachandran <aswin.gokulachandran87@gmail.com>
2015-08-18 17:00:22 -0500

    updating untracked files

--- src/pkg_Arrays/LargestNumber.java
+++ src/pkg_Arrays/LargestNumber.java
@@ -16,7 +16,7 @@ public class LargestNumber {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {1,2};
+		int[] nums = {9,985,3,2};
 		LargestNumber ln = new LargestNumber();
 		System.out.println(ln.largestNumber(nums));
 
@@ -28,7 +28,13 @@ public class LargestNumber {
 			strs[i] = String.valueOf(nums[i]);
 		}
 		
-		Arrays.sort(strs);
+//		Arrays.sort(strs), new Comparator<String>() {
+//			public int compare(String s1, String s2) {
+//				String lr = s1 + s2;
+//				String rl = s2 + s1;
+//				return -lr.compareTo(rl);
+//			}
+//		});
 		StringBuilder sb = new StringBuilder();
 		for(String s:strs)
 			sb.append(s);
