56f097faba509c0175e95da4bfacd38396b7066a
Aswin Gokulachandran <aswin.gokulachandran87@gmail.com>
2015-08-19 14:23:02 -0500

    Shifting array - Amazon question

--- src/pkg_Arrays/ArrangeAlternatively.java
+++ src/pkg_Arrays/ArrangeAlternatively.java
@@ -3,6 +3,8 @@
  */
 package pkg_Arrays;
 
+import java.util.ArrayList;
+
 /**
  * @author aswin
  *
@@ -15,39 +17,47 @@ public class ArrangeAlternatively {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrangeAlternatively aa = new ArrangeAlternatively();
-		int[] ip = {1,2,2};
+		int[] ip = {1,2,3,4};
 		aa.arrange(ip);
 
 	}
 	
 	void arrange(int[] nums) {
-		int odd = 0, even = 0;
-		for(int i =0; i<nums.length; i++) {
-			if(nums[i]%2 == 0)
-				even++;
-			else
-				odd++;
-		}
 		
-		int[] odda = new int[odd];
-		int[] evena = new int[even];
-		int oc = 0, ec = 0;
+		ArrayList<Integer> oddlist = new ArrayList<Integer>();
+		ArrayList<Integer> evenlist = new ArrayList<Integer>();
+				
 		for(int i=0; i<nums.length ;i++) {
 			if(nums[i] %2 == 0) {
-				evena[ec] = nums[i];
-				ec++;
+				evenlist.add(nums[i]) ;				
 			}else {
-				odda[oc] = nums[i];
-				oc++;
+				oddlist.add(nums[i]);
 			}
 		}
-		oc = 0;
-		for(int i=0; i<nums.length && oc < odd; i+=2, oc++) {
-			nums[i] = odda[oc];  
+		
+		System.out.print("Odd list: "); 
+		for(int k : oddlist) {			
+			System.out.print(k +" ");			
 		}
-		ec = 0;
-		for(int i = 1; i < nums.length && ec < even; i+=2, ec++) {
-			nums[i] = evena[ec];
+		System.out.println();
+		System.out.print("even list: "); 
+		for(int k : evenlist) {			
+			System.out.print(k +" ");			
+		}
+		System.out.println();
+		
+		int ec = 0;
+		for(int i=0; i<nums.length && ec < evenlist.size(); i++) {
+			System.out.println("Inserting " + evenlist.get(ec) + " into the " + 2*i +" th position");
+			nums[i*2] = evenlist.remove(ec);
+			ec++;
+			System.out.println("List size: "+evenlist.size());
+			System.out.println("Nums size: "+ nums.length);
+		}
+		int oc = 0;
+		for(int i = 1; i < nums.length && i < oddlist.size(); i+=2) {
+			System.out.println("Inserting " + oddlist.get(oc) + " into the " + i +" th position");
+			nums[i] = oddlist.remove(oc++);
 		}
 		
 		for(int j = 0; j<nums.length; j++) {
