b3572e70ec6c0d9440bb875a3fe1e9850690c163
Aswin Gokulachandran <aswin.gokulachandran87@gmail.com>
2016-09-02 22:00:48 -0700

    Added Number Max, Smallest Diff java files

---
+++ src/pkg_BitManip/NumberMax.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pkg_BitManip;
+
+/**
+ * @author Aswin
+ *
+ */
+public class NumberMax {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
